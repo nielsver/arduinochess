25a782cdaa773715de455954bb31d9df109738c4
johan525 <95874895+johan525@users.noreply.github.com>
2022-05-17 17:50:25 +0200

    finale werkende versie

--- arduino_chess/arduino_chess.ino
+++ arduino_chess/arduino_chess.ino
@@ -1,11 +1,12 @@
+//project arduinochess gemaakt door Johan Hoeben en Niels Vervoort
 #include <LiquidCrystal_I2C.h>
 #include <ShiftDisplay.h>
 #include <Wire.h>
 LiquidCrystal_I2C lcd(0x27,16,2);
 ShiftDisplay display(COMMON_CATHODE, 3);
-int buzzer = 4;
-int led1 = 7;
-int led2 = 6;
+int buzzer = 9;
+int led1 = 8;
+int led2 = 12;
 int button1 = 2;
 int button2 = 3;
 int resetbutton = 11;
@@ -18,10 +19,13 @@ int game = 0;
 int i = 0;
 int statu = 0;
 int stat = 0;
-int ctr1 = 3559;
-int ctr2 = 3559;
+int ctr1 = 599;
+int ctr2 = 599;
 int huidigspel = 0;
 int speeltijd = 0;
+int startpressed = 0;
+int endpressed = 0;
+int holdtime = 0;
 
 void setup() {
   lcd.init();//initialize LCD
@@ -37,7 +41,6 @@ void setup() {
 
 void loop() { 
   reset();
-  testdisplay();
   statistieken();
   aantalspellen++;
   while(game == 0){
@@ -48,10 +51,18 @@ void loop() {
     langstespel = huidigspel;
   }
   gemiddeldespeelduur = speeltijd / aantalspellen;
+  
   if(game == 2){
     gewonnenwit++;
   }
-  else{
+  
+  else if(game ==1){
+    gewonnenzwart++;
+  }
+  else if(game == 3){
+    gewonnenwit++;
+  }
+  else if(game == 4){
     gewonnenzwart++;
   }
   finish(game, huidigspel);
@@ -75,7 +86,7 @@ void statistieken() {
     delay(5000);
     lcd.clear();
     if( gewonnenwit > gewonnenzwart) {
-      lcd.print(String("white has won: "));
+      lcd.print(String("white is winning"));
       lcd.setCursor(0,1);
       lcd.print(String(gewonnenwit)+String("-")+String(gewonnenzwart)); //eventueel 63%w/r
       delay(5000);
@@ -87,7 +98,7 @@ void statistieken() {
       delay(5000);
     }
     else {
-      lcd.print(String("black has won:"));
+      lcd.print(String("black is winning"));
       lcd.setCursor(0,1);
       lcd.print(String(gewonnenzwart)+String("-")+String(gewonnenwit));
       delay(5000);
@@ -117,18 +128,25 @@ int spelen() {
       stat = ctrblack();
     }
   }
-  if(statu == 1){
+  if(statu == 1){//wit verliest op tijd
     return 1;
   }
-  else {
+  
+  else if(stat ==1){//zwart verliest op tijd
     return 2;
+  }
+  else if(statu == 3){// wit wint schaakmat
+    return 3;
+  }
+  else if(stat == 3){//zwart wint schaakmat
+    return 4;
   }
 }
 
 int ctrwhite(){
   int buttonstate1 = digitalRead(button1);
   lcd.clear();
-  lcd.print(String("wit aan zet"));
+  lcd.print(String("white to move"));
   int test = 0;
   while (buttonstate1 == LOW && ctr1 >= 0){
     buttonstate1 = digitalRead(button1);
@@ -138,6 +156,17 @@ int ctrwhite(){
     huidigspel++;
     ctr1--;    
   }
+  while(buttonstate1 == HIGH){
+    buttonstate1 = digitalRead(button1);
+    startpressed++;
+    delay(1);    
+  }
+  if(startpressed > 3000){
+    startpressed = 0;
+    return 3;
+  }
+  startpressed = 0;
+
   if( ctr1 > 0){
     digitalWrite(led1, LOW);
     lcd.clear();
@@ -151,7 +180,7 @@ int ctrwhite(){
 int ctrblack(){
   int buttonstate2 = digitalRead(button2);
   lcd.clear();
-  lcd.print(String("zwart aan zet"));
+  lcd.print(String("black to move"));
   while (buttonstate2 == LOW && ctr2 >= 0){
     buttonstate2 = digitalRead(button2);
     digitalWrite(led2,HIGH);
@@ -160,6 +189,17 @@ int ctrblack(){
     huidigspel++;
     ctr2--;
   }
+    while(buttonstate2 == HIGH){
+    buttonstate2 = digitalRead(button2);
+    startpressed++;
+    delay(1);
+  }
+  if(startpressed > 3000){
+    startpressed = 0;
+    return 3;
+  }
+
+  startpressed = 0;
   if(ctr2 > 0) {
     digitalWrite(led2, LOW);
     lcd.clear();
@@ -172,14 +212,14 @@ int ctrblack(){
   
 void finish(int game, int huidigspel) {
   lcd.clear();
-  lcd.print(String("het spel is"));
+  lcd.print(String("the game is"));
   lcd.setCursor(0,1);
-  lcd.print(String("gedaan"));
+  lcd.print(String("finished"));
   delay(3000);
   Displaylcd(huidigspel);
   lcd.clear();
   if(game == 2){
-    lcd.print(String("white has won!!!"));
+    lcd.print(String("white has won"));
     tone(buzzer, 1000, 5000);
     for(int i = 0; i<5;i++){
     digitalWrite(led1, HIGH);
@@ -190,8 +230,8 @@ void finish(int game, int huidigspel) {
     digitalWrite(led2, LOW);
     }
   }
-  else{
-    lcd.print(String("black has won!!!"));
+  else if(game ==1){
+    lcd.print(String("black has won"));
     tone(buzzer, 1000, 5000);
     for(int i = 0; i<5;i++){
     digitalWrite(led1, HIGH);
@@ -202,6 +242,32 @@ void finish(int game, int huidigspel) {
     digitalWrite(led2, LOW);
   }
   }
+
+  else if(game == 3){
+    lcd.print(String("white has won"));
+    tone(buzzer, 1000, 5000);
+    for(int i = 0; i<5;i++){
+    digitalWrite(led1, HIGH);
+    delay(500);
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, HIGH);
+    delay(500);
+    digitalWrite(led2, LOW);
+    }
+  }
+  else if(game == 4){
+    lcd.print(String("black has won"));
+    tone(buzzer, 1000, 5000);
+    for(int i = 0; i<5;i++){
+    digitalWrite(led1, HIGH);
+    delay(500);
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, HIGH);
+    delay(500);
+    digitalWrite(led2, LOW);
+  }
+  }
+  
  
 
   
@@ -209,8 +275,8 @@ void finish(int game, int huidigspel) {
 }
 
 void reset(){
-  ctr1 = 10;
-  ctr2 = 10;
+  ctr1 = 599;
+  ctr2 = 599;
   game = 0;
   statu = 0;
   stat = 0;
@@ -220,35 +286,36 @@ void reset(){
 }
 
 void Displaylcd(int seconden){
-  int minuten
+  int minuten;
   if (seconden < 60)
   {
-    lcd.print(String("0 : ")+String(seconden) + String(" minuten"));
+    lcd.print(String("0min ")+String(seconden)+ String("sec"));
   }
   else
   { 
     minuten = seconden / 60;
     seconden = seconden % 60;
-    lcd.print(String(minuten) + String(" : ") + String(seconden) + String(" minuten));
+    lcd.print(String(minuten) + String("min ") + String(seconden) + String("sec"));
   }
 }
 void Display(int seconden) {
-    int minuten
+  int minuten;
   if (seconden < 60)
   {
     display.show(seconden, 1000, ALIGN_RIGHT); // store number and show it for 400ms
     display.setDot(1, true); // add dot to stored number
-    display.show(1000); // show number with dot for 400ms
+    //display.show(1000); // show number with dot for 400ms
   }
   else
   { 
-    minuten = seconden / 60;
+    //sec = 599
+    minuten = seconden / 60; //min = 9
     seconden = seconden % 60;
     minuten = minuten * 100;
-    minuten = minuten + seconden
+    minuten = minuten + seconden;
     display.show(minuten, 1000, ALIGN_RIGHT); // store number and show it for 400ms
     display.setDot(1, true); // add dot to stored number
-    display.show(1000); // show number with dot for 400ms
+    //display.show(1000); // show number with dot for 400ms
   }
 }
   
